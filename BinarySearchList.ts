00f28bc86806d5f2ad65a4940c877523f09a40f3
grellheist <grellheist@tutanota.com>
2023-03-24 14:25:58 -0300

    Updated

---
+++ BinarySearchList.ts
@@ -0,0 +1,20 @@
+export default function binarySearch(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    
+    do{
+        const middlePoint = Math.floor(low + (high - low) / 2);
+        const value = haystack[middlePoint];
+
+        if (value === needle){
+            return true;
+        } else if (value > needle){
+            high = middlePoint;
+        } else {
+            low = middlePoint + 1;
+        }
+
+    } while (low < high);
+
+    return false
+}
